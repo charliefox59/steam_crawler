b10437dadeca5adaf2e77afe3b205027981161fb
= <charlie.kelvin.fox@gmail.com>
2025-03-26 11:39:05 +0000

    Added batch processing, added 3 unit tests

--- steam_crawler.py
+++ steam_crawler.py
@@ -3,76 +3,78 @@ import requests
 import json
 from datetime import datetime
 import uuid
+from datetime import date
 
 class steam_crawler():
 
-    def __init__(self, app_id: int, game_name: str, franchise_name : str, n_reviews: int) -> None:
+    def __init__(self, app_id: int, game_name: str, franchise_name : str, batch_size: int,
+                 date_interval: tuple[str, str] | None = None) -> None:
         '''
         Initialise variables used across multiple functions. 
-        Generate base url for steam getreviews to avoid redundant code.
+
+        Generate base url for get_reviews
         '''
         self.app_id = app_id
         self.game_name = game_name
         self.franchise_name = franchise_name
-        self.n_reviews = n_reviews
+        self.batch_size = batch_size
+        self.date_interval = date_interval
 
+        self.params = {
+            'json' : 1,
+            'day_range' : 9223372036854775807,
+            'num_per_page' : 100
+            }
         self.source = "steam" 
         self.url = f"https://store.steampowered.com/appreviews/{self.app_id}"
         
-    def get_reviews(self, params : dict): 
-            '''Calls steam API and returns a list of dictionaries of raw data, passes the arguments within "params" to the call.'''
+    def get_reviews(self, params : dict) -> dict: 
+            '''
+            Calls steam API and returns a list of dictionaries of raw data.
+
+            Return response in as dictionary
+            '''
             return requests.get(self.url, params = params).json()
 
-    def get_n_reviews(self): 
+    def write_json(self, data : list[dict], filename : str) -> None:
         '''
-        Collect batches of steam reviews data until we have at least n review
-        These batches are usually of size 100, from 'num_per_page' : 100
-        Some batches are smaller due to steam automatically filtering off-topic reviews (aka "Review Bombs")
-        Setting day_range = 9223372036854775807 removes the filter that only takes the last 365 days worth of reviews
-        Return a list of n dictionaries for n reviews
+        Write the formatted data to a .json file in "steam_crawler/output" folder
         '''
-        raw_review_data = []
-        n_reviews = 0
-        params = {
-            'json' : 1,
-            'day_range' : 9223372036854775807,
-            'cursor' : '*',
-            'num_per_page' : 100
-            }
-        ids = []
-        print(f"Fetching...")
-        while n_reviews < self.n_reviews:
-            response = self.get_reviews(params)
-            
-            params["cursor"] = response["cursor"]
-            n_reviews = len(raw_review_data)
+        path = "crawler/output/" + filename + ".json"
+        with open(path, "w") as f:
+            f.write(json.dumps(data))
 
-            raw_review_data += response["reviews"]
 
-        return raw_review_data[:self.n_reviews]
-
-    def generate_uuid(self, base_id: str):
+    def generate_uuid(self, base_id: str) -> str:
         '''
-        Use uuid5 to return a UUID based on a string, this UUID is reproducable if you input the same string. 
-        Choose DNS namespace for generating uuids
+        Input string (base_id), use uuid5 to return a UUID.
+        
+            UUID is reproducable if you input the same string. 
+
+            Choose DNS namespace for generating UUIDs.
+
+        Return UUID string
         '''
         return uuid.uuid5(uuid.NAMESPACE_DNS, base_id).hex
 
-    def parse_timestamp(self, timestamp: int):
-        '''Input timestamp from steam data and return a date string in "YYYY-MM-DD format"'''
+    def parse_timestamp(self, timestamp: int) -> str:
+        '''
+        Input timestamp from steam data and 
+        
+        Return a date string in "YYYY-MM-DD format"
+        '''
         return str(datetime.fromtimestamp(timestamp).date())
 
-    def format_data(self):
+    def format_data(self, reviews_dict : list[dict]) -> list[dict]:
         '''
         Input list of dictionaries containing reviews from the steam API.
-        Generate UUIDs for the review ID and the author ID.
-        Parse the timestamp into date format.
-        Choose "timestamp_created" for date (instead of "timestamp_updated").
-        Choose "playtime_at_review" for hours (instead of "playtime_forever").
-        Add other relevant fields pulled from the crawled user reviews.
+            Generate UUIDs for the review ID and the author ID.
+            Parse the timestamp into date format.
+            Choose "timestamp_created" for date (instead of "timestamp_updated").
+            Choose "playtime_at_review" for hours (instead of "playtime_forever").
+            Add other relevant fields pulled from the crawled user reviews.
         Return list of dictionaries in the desired format.
         '''
-        reviews_dict = self.get_n_reviews()
         output = []
 
         for data in reviews_dict: 
@@ -90,22 +92,74 @@ class steam_crawler():
                 "franchise": self.franchise_name,
                 "gameName": self.game_name
                 })
-        sorted_output = sorted(output, key=lambda x: (datetime.strptime(x['date'], '%Y-%m-%d'),x["id"]))
-        return sorted_output
+        sorted_output = sorted(output, key=lambda x: (datetime.strptime(x['date'], '%Y-%m-%d'),x["id"]), reverse=True)
+
+        if self.date_interval is not None: 
+            return self.filter_dates(sorted_output)
+        else: 
+            return sorted_output
+    
+    def filter_dates(self, data: list[dict]) -> list[dict]:
+        '''
+        Input formatted reviews data
+
+        Return filtered reviews beween 2 dates with format "YYYY-MM-DD"
+        ''' 
+        start_date, end_date = date.fromisoformat(self.date_interval[0]), date.fromisoformat(self.date_interval[1])
+        return [review for review in data if start_date <= date.fromisoformat(review["date"]) <= end_date]
+
+
+    
         
-    def write_json(self):
-        '''
-        Write the formatted data to a .json file in "steam_crawler/output" folder
-        Name the file by the game name, replacing spaces with underscores.
-        '''
-        filename = self.game_name.replace(" ", "_") + "_reviews.json"
-        with open("steam_crawler/output/" + filename, "w") as f:
-            f.write(json.dumps(self.format_data()))
+    def process_batch(self, batch_size : int) -> list[dict]:
+        raw_review_data = []
+        review_count = 0
+        
+        while review_count < batch_size:
+            response = self.get_reviews(self.params)
+            raw_review_data += response["reviews"]
 
+            #Update cursor for next batch and the number of reviews
+            self.params["cursor"] = response["cursor"]
+            review_count = len(raw_review_data)
 
-steam_crawler(app_id = 1382330,
-              game_name = "Persona 5 Strikers",
+        return self.format_data(raw_review_data)
+    
+    def get_all_reviews(self) -> list[dict] : 
+        '''
+        Collect batches of steam reviews data until we have at all reviews.
+
+            These batches are usually of size 100, from 'num_per_page'.
+
+            Some batches are smaller due to steam automatically filtering off-topic reviews (aka "Review Bombs").
+    
+            Setting day_range = 9223372036854775807 removes the filter that only takes the last 365 days worth of reviews.
+
+        Return a list of n dictionaries for n reviews
+        '''
+        self.params["cursor"] = "*"
+        total_reviews = self.get_reviews(params)["query_summary"]["total_reviews"] 
+        batch_sizes = [batch for batch in range(self.batch_size, total_reviews, self.batch_size)] + [total_reviews % self.batch_size]
+        batch_count = 0
+        for batch_size in batch_sizes:
+            batch_count += 1
+            print(f"Running batch {batch_count} with {batch_size} reviews")
+            
+            self.write_json(self.process_batch(batch_size), 
+                            f"{self.game_name}_{batch_count}")
+
+        
+crawler = steam_crawler(app_id = 1382330,
+              game_name = "Persona_5_Strikers",
               franchise_name = "ATLUS",
-              n_reviews = 5000
-              ).write_json()
+              batch_size = 5000)
+              #date_interval=("2022-01-01","2023-01-01"))
 
+params = {
+            'json' : 1,
+            'day_range' : 9223372036854775807,
+            'cursor' : '*',
+            'num_per_page' : 100
+            }
+
+response = crawler.get_all_reviews()
--- steam_crawler_test.py
+++ steam_crawler_test.py
@@ -1,21 +1,22 @@
-from steam_crawler import steam_crawler
+import steam_crawler
 import unittest, os, sys
 from unittest.mock import MagicMock,patch,call
+import requests
 sys.path.append(os.path.realpath(".."))
 sys.path.append(os.path.realpath("."))
 
 class test_crawler(unittest.TestCase):
 
-    # def setUp(self):
-    #     mock_id = MagicMock(int)
-    #     mock_game_name = MagicMock(str)
-    #     mock_franchise_name = MagicMock(str)
-    #     mock_n_reviews = MagicMock(int)
-    #     self.uut = steam_crawler(app_id = mock_id, 
-    #                          game_name = mock_game_name,
-    #                          franchise_name = mock_franchise_name,
-    #                          n_reviews = mock_n_reviews)
-    #     return super().setUp()
+    def setUp(self):
+        app_id = 1382330
+        game_name = "Persona 5 Strikers"
+        franchise_name = "ATLUS"
+        batch_size = 5000
+        self.uut = steam_crawler.steam_crawler(app_id = app_id, 
+                             game_name = game_name,
+                             franchise_name = franchise_name,
+                             batch_size = batch_size)
+        return super().setUp()
     # 7 Functions: 
     #__init__() (6 lines of code)
 
@@ -23,24 +24,62 @@ class test_crawler(unittest.TestCase):
         mock_id = MagicMock(int)
         mock_game_name = MagicMock(str)
         mock_franchise_name = MagicMock(str)
-        mock_n_reviews = MagicMock(int)
-        _uut = steam_crawler(app_id = mock_id, 
+        mock_batch_size = MagicMock(int)
+
+        _uut = steam_crawler.steam_crawler(app_id = mock_id, 
                              game_name = mock_game_name,
                              franchise_name = mock_franchise_name,
-                             n_reviews = mock_n_reviews)
+                             batch_size = mock_batch_size)
         self.assertEqual(_uut.app_id, mock_id)
         self.assertEqual(_uut.game_name, mock_game_name)
         self.assertEqual(_uut.franchise_name, mock_franchise_name)
-        self.assertEqual(_uut.n_reviews, mock_n_reviews)
+        self.assertEqual(_uut.batch_size, mock_batch_size)
 
     #get_reviews(params) (1 line of code )
-
-    #get_n_reviews() (18 lines of code)
+    @patch("steam_crawler.requests.get")
+    def test_get_reviews(self, get_requests_patch):
+        params = {
+            'json' : 1,
+            'day_range' : 9223372036854775807,
+            'cursor' : '*',
+            'num_per_page' : 100
+            }
+        self.assertEqual(get_requests_patch(self.uut.url,params).json(),
+                         self.uut.get_reviews(params))
 
     #generate_uuid(base_id) (1 lines of code)
+    @patch("steam_crawler.uuid")
+    def test_generate_uuid(self, uuid_patch):
+        base_id_mock = MagicMock(int)
+        r = self.uut.generate_uuid(base_id_mock)
 
+        self.assertEqual(r, 
+            uuid_patch.uuid5(uuid_patch.NAMESPACE_DNS, base_id_mock).hex)
+        
     #parse_timestamp(timestamp) (1 lines of code)
+    @patch("steam_crawler.datetime")
+    def test_parse_timestamp(self, datetime_patch): 
+        timestamp_mock = MagicMock(int)
+        timestamp_mock.return_value = 1652341169
+
+        r = self.uut.parse_timestamp(timestamp_mock)
+        self.assertEqual(r, 
+            str(datetime_patch.fromtimestamp(timestamp_mock).date()))
 
     #format_data() (20 lines of code)
 
     #write_json() (3 lines of code)
+
+    #def test_write_json(self):
+
+    #get_all_reviews() (18 lines of code)
+    @patch("steam_crawler.steam_crawler.write_json")
+    @patch("steam_crawler.steam_crawler.format_data")
+    @patch("steam_crawler.steam_crawler.get_reviews")
+    def test_get_all_reviews(self, get_reviews_patch, format_data_patch, write_json_patch,):
+        
+        
+        r = self.uut.get_all_reviews()
+        
+        
+
